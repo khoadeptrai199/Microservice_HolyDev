a129f4c2b59af1b4504f60e2e14385496f999a0d
MinhKhoa84 <minhkhoathsptv@gmail.com>
2025-09-12 23:37:54 +0700

    order-service

---
+++ order-service/src/main/java/com/holydev/order_service/model/Order.java
@@ -0,0 +1,23 @@
+package com.holydev.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String product;
+    private Double price;
+}
