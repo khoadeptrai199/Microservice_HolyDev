17ac463d70e3fe383cdf7770d3aa035d38fb9ac4
MinhKhoa84 <minhkhoathsptv@gmail.com>
2025-09-12 23:31:56 +0700

    user-service

---
+++ users-service/src/main/java/com/holydev/users_service/model/User.java
@@ -0,0 +1,22 @@
+package com.holydev.users_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users") // cho ai chưa biết thì users nhớ thêm s nhé, chứ ko thêm s la thanh user keyword trong Sql và khi chay code se bi loi
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
